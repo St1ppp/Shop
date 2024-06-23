9f5a6adf7771cd390f2d64399375a3bf19f584fa
St1ppp <thest1p2005@gmail.com>
2024-06-23 14:40:09 +1000

    first models

--- main/views.py
+++ main/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'main/index.html')
+    context = {
+        'title': 'Home',
+        'content': 'This is the home page',
+    }
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    context = {
+        'title': 'About',
+        'content': 'О нас',
+        'text_on_page': 'Это крутой шоп, братанчик',
+    }
+    return render(request, 'main/about.html', context)
